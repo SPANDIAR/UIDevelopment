c95ba19a8c563102c0b712bee4b3171e1ab5dd0a
sankar_pandiarajan <spandiarajan@apple.com>
2020-03-25 13:12:20 -0700

    JS Objects

--- udemy-mosh-js-prj/index.js
+++ udemy-mosh-js-prj/index.js
@@ -1,14 +1,1 @@
 console.log('this message is from my js file');
-
-printStars(6);
-
-function printStars(count){
-  let star;
-  for(let i=1; i<=count; i++){
-    star='';
-    for(let j=1; j<=i; j++) {
-      star += '*';
-    }
-    console.log(star);
-  }
-}
---
+++ udemy-mosh-js-prj/understandingObjects.js
@@ -0,0 +1,11 @@
+// understanding multiple concepts in JS Objects
+
+// Factory method of initializing objects
+
+const car = {
+  brand: 'Mercedez',
+  model: 'C Class',
+  type: 'Convertible'
+};
+
+// Constructor method of initializing objects

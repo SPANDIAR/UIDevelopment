bc4eadfbb3dd86966f0a24d4e42caccdceae512d
sankar_pandiarajan <spandiarajan@apple.com>
2020-03-26 18:55:09 -0700

    refactored

--- udemy-mosh-js-prj/arraysExercise2.js
+++ udemy-mosh-js-prj/arraysExercise2.js
@@ -2,9 +2,7 @@
 sourceArray = [1, 3, 2, 4, 5];
 console.log('Source Array: ' + sourceArray);
 
-movePositions(sourceArray,1,5); // [1, 3, 5, 2, 4]
-//movePositions(sourceArray,2,-2); // [2, 1, 3, 5, 4]
-//movePositions(sourceArray,1,3); // [2, 1, 3, 5, 4]
+movePositions(sourceArray,2,-2);
 
 function movePositions(source, elementIndex, offset){
   let output = [...source];
@@ -17,19 +15,8 @@ function movePositions(source, elementIndex, offset){
           return;
     }
 
-
-    let elementToMove = source[elementIndex];
-    let newPosition = elementIndex + offset;
-    console.log(elementToMove + ' ' + newPosition);
-
-    if(offset > 0) {
-      output.splice(newPosition+1, 0, elementToMove);
-      output.splice(elementIndex, 1);
-    }
-    else if (offset < 0){
-      output.splice(newPosition, 0, elementToMove);
-      output.splice(elementIndex+1, 1);
-    }
+    let elementToMove = output.splice(elementIndex, 1);
+    output.splice(elementIndex + offset, 0, elementToMove);
 
     console.log('Rearranged Array is: ' + output);
 }
